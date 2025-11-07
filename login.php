b7ba58ca13bf1f22fe8c8841d1f29c0e28debd41
insxanityMAD <christian.malinao335@gmail.com>
2025-11-07 19:58:02 +0800

    hi

--- login.php
+++ login.php
@@ -59,8 +59,7 @@ echo "inserted successfully";
 <input type ="submit" name = "clicked" > 
 <p2> Already have an account? </p2> <a href = "sign-in.php"> Click this to sign-up.</a>
 
-//this is a comment....
-//tngina mo mark!! HAHHAHAHA sample rani ha
+
 
 </form>
 </body>
